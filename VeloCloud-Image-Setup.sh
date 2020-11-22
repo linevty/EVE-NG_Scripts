2eec41695658cefa85bf8677466af6b24c1dd5e3
linevty <steve@Steves-MacBook-Pro.local>
2020-11-22 23:17:27 +0000

    VeloCloud Script updated

--- VeloCloud-Image-Setup.sh
+++ VeloCloud-Image-Setup.sh
@@ -10,13 +10,28 @@ OrchSrcPath="~/velo343/veloorch"
 GWSrcPath="~/velo343/velogw"
 EdgeSrcPath="~/velo343/veloedge"
 
-mkdir /opt/unetlab/addons/qemu/template1-veloorch-3.2.2
+# End of Configuration
+
+# ------------------------------
+
+# Create template folders
+
+mkdir /opt/unetlab/addons/qemu/template-veloorch-3.2.2
 mkdir /opt/unetlab/addons/qemu/template-velogw-3.2.2
 mkdir /opt/unetlab/addons/qemu/template-veloedge-3.2.2
+
+# Copy qcow2 files into the template directory
 
 cp $OrchSrcPath/*.qcow2 /opt/unetlab/addons/qemu/template-veloorch-3.2.2/
 cp $GWSrcPath/*.qcow2 /opt/unetlab/addons/qemu/template-veloorch-3.2.2/
 cp $EdgeSrcPath/*.qcow2 /opt/unetlab/addons/qemu/template-veloorch-3.2.2/
+
+# Build each image by:
+#  - symlinking the files from the template directory
+#  - Creating the user-data and meta-data files including hostnames
+#  - Creating ISO file including user-data and meta-data files
+
+# Build Orchestrator Image 1
 
 mkdir /opt/unetlab/addons/qemu/veloorch-3.4.3-01
 
@@ -40,8 +55,6 @@ echo instance-id: vco-01 >> /opt/unetlab/addons/qemu/veloorch-3.4.3-01/meta-data
 echo local-hostname: vco-01 >> /opt/unetlab/addons/qemu/veloorch-3.4.3-01/meta-data
 
 genisoimage -output /opt/unetlab/addons/qemu/veloorch-3.4.3-01/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloorch-3.4.3-01/user-data /opt/unetlab/addons/qemu/veloorch-3.4.3-01/meta-data
-
-
 
 # - Build Orchestrator Image 2
 
@@ -68,8 +81,6 @@ echo local-hostname: vc0-02 >> /opt/unetlab/addons/qemu/veloorch-3.4.3-02/meta-d
 
 genisoimage -output /opt/unetlab/addons/qemu/veloorch-3.4.3-02/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloorch-3.4.3-02/user-data /opt/unetlab/addons/qemu/veloorch-3.4.3-02/meta-data
 
-
-
 # - Build GW Image 1
 
 mkdir /opt/unetlab/addons/qemu/velogw-3.4.3-01
@@ -84,8 +95,6 @@ echo local-hostname: vcg-01 >> /opt/unetlab/addons/qemu/velogw-3.4.3-01/meta-dat
 
 genisoimage -output /opt/unetlab/addons/qemu/velogw-3.4.3-01/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/velogw-3.4.3-01/user-data /opt/unetlab/addons/qemu/velogw-3.4.3-01/meta-data
 
-
-
 # - Build GW Image 2
 
 mkdir /opt/unetlab/addons/qemu/velogw-3.4.3-02
@@ -97,8 +106,6 @@ echo password: password1  >> /opt/unetlab/addons/qemu/velogw-3.4.3-02/user-data
 
 echo instance-id: vcg-02 >> /opt/unetlab/addons/qemu/velogw-3.4.3-02/meta-data
 echo local-hostname: vcg-02 >> /opt/unetlab/addons/qemu/velogw-3.4.3-02/meta-data
-
-
 
 # - Build GW Image 3
 
@@ -112,8 +119,6 @@ echo password: password1  >> /opt/unetlab/addons/qemu/velogw-3.4.3-03/user-data
 echo instance-id: vcg-03 >> /opt/unetlab/addons/qemu/velogw-3.4.3-03/meta-data
 echo local-hostname: vcg-03 >> /opt/unetlab/addons/qemu/velogw-3.4.3-03/meta-data
 
-
-
 # - Build GW Image 4
 
 mkdir /opt/unetlab/addons/qemu/velogw-3.4.3-04
@@ -125,10 +130,6 @@ echo password: password1  >> /opt/unetlab/addons/qemu/velogw-3.4.3-04/user-data
 
 echo instance-id: vcg-04 >> /opt/unetlab/addons/qemu/velogw-3.4.3-04/meta-data
 echo local-hostname: vcg-04 >> /opt/unetlab/addons/qemu/velogw-3.4.3-04/meta-data
-
-
-
-
 
 # - Build Edge Image 1
 
@@ -144,9 +145,6 @@ echo local-hostname: vce-01 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-01/meta-d
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-01/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-01/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-01/meta-data
 
-
-
-
 # - Build Edge Image 2
 
 mkdir /opt/unetlab/addons/qemu/veloedge-3.4.3-02
@@ -160,8 +158,6 @@ echo instance-id: vce-02 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-02/meta-data
 echo local-hostname: vce-02 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-02/meta-data
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-02/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-02/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-02/meta-data
-
-
 
 # - Build Edge Image 3
 
@@ -177,7 +173,6 @@ echo local-hostname: vce-03 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-03/meta-d
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-03/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-03/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-03/meta-data
 
-
 # - Build Edge Image 4
 
 mkdir /opt/unetlab/addons/qemu/veloedge-3.4.3-04
@@ -191,7 +186,6 @@ echo instance-id: vce-04 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-04/meta-data
 echo local-hostname: vce-04 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-04/meta-data
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-04/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-04/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-04/meta-data
-
 
 # - Build Edge Image 5
 
@@ -207,8 +201,6 @@ echo local-hostname: vce-05 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-05/meta-d
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-05/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-05/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-05/meta-data
 
-
-
 # - Build Edge Image 6
 
 mkdir /opt/unetlab/addons/qemu/veloedge-3.4.3-06
@@ -222,7 +214,6 @@ echo instance-id: vce-06 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-06/meta-data
 echo local-hostname: vce-06 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-06/meta-data
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-06/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-06/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-06/meta-data
-
 
 # - Build Edge Image 7
 
@@ -238,8 +229,6 @@ echo local-hostname: vce-07 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-07/meta-d
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-07/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-07/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-07/meta-data
 
-
-
 # - Build Edge Image 8
 
 mkdir /opt/unetlab/addons/qemu/veloedge-3.4.3-08
@@ -254,6 +243,6 @@ echo local-hostname: vce-08 >> /opt/unetlab/addons/qemu/veloedge-3.4.3-08/meta-d
 
 genisoimage -output /opt/unetlab/addons/qemu/veloedge-3.4.3-08/cdrom.iso -volid cidata -joliet -rock /opt/unetlab/addons/qemu/veloedge-3.4.3-08/user-data /opt/unetlab/addons/qemu/veloedge-3.4.3-08/meta-data
 
-
+# Run standard eve-ng fixpermissions script to finish
 
 /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
